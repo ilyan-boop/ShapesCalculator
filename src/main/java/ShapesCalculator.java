915547d570b9583cf62289bdd807455c5bf4e5cc
Zachary Molinyawe <zachary_molinyawe@dlsu.edu.ph>
2026-06-03 21:02:40 +0800

    10th Commit: Added the inputs for filled, and started the switch case covering the square and rectangle class.

--- src/main/java/ShapesCalculator.java
+++ src/main/java/ShapesCalculator.java
@@ -5,7 +5,7 @@ public class ShapesCalculator {
 
         int choice = 0;
 
-        // Loop until user chooses option 6 (Exit)
+        //UNDERSTAND: While loop until user inputs 6 (exit)
         while (choice != 6) {
             IO.println("\n=========================================");
             IO.println("           2D SHAPES CALCULATOR          ");
@@ -19,7 +19,7 @@ public class ShapesCalculator {
             IO.println("=========================================");
             IO.print("Enter your choice (1-6): ");
 
-            // Convert input string to an integer choice
+            //UNDERSTAND: This block will validate the input
             try {
                 choice = Integer.parseInt(IO.readln().trim());
             } catch (NumberFormatException e) {
@@ -32,11 +32,49 @@ public class ShapesCalculator {
                 break;
             }
 
+            //UNDERSTAND: This block will validate whether the input is 1-6
             if (choice < 1 || choice > 6) {
                 IO.println("Invalid choice! Please pick a number from 1 to 6.");
                 continue;
             }
 
+            //DECISION: This was put here since it is common between all classes
+            IO.print("Should the shape be filled? (true/false): ");
+            boolean filled = Boolean.parseBoolean(IO.readln().trim());
+
+            switch (choice) {
+                case 1: //UNDERSTAND: This is for the Square class
+                    IO.print("Enter the side of the squre: ");
+                    double side = Double.parseDouble(IO.readln().trim());
+
+                    Square square = new Square(side, filled);
+
+                    IO.println("\n--- SQUARE RESULTS ---");
+                    IO.println("Area: " + square.CalculateArea());
+                    IO.println("Perimeter: " + square.CalculatePerimeter());
+                    square.printSquare();
+                    break;
+
+                case 2://UNDERSTAND: This is for the Rectangle class
+                    IO.print("Enter length: ");
+                    double length = Double.parseDouble(IO.readln().trim());
+                    IO.println("Enter width: ");
+                    double width = Double.parseDouble(IO.readln().trim());
+
+                    Rectangle rectangle = new Rectangle(length, width, filled);
+                    IO.println("\n--- RECTANGLE RESULTS ---");
+                    IO.println("Area: " + rectangle.CalculateArea());
+                    IO.println("Perimeter: " + rectangle.CalculatePerimeter());
+                    rectangle.printRectangle();
+                    break;
+
+                case 3:
+
+                case 4:
+
+                case 5:
+
+            }
         }
     }
 }
